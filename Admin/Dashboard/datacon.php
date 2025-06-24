3983db6177d7a7cca6b6b6426d626df2678196b6
znyadzi <125977854+znyadzi@users.noreply.github.com>
2025-06-24 12:05:02 +0000

    Cleaning up the starters

--- Admin/Dashboard/datacon.php
+++
@@ -1,7 +0,0 @@
-<?php
- $dbServername= "localhost";
- $dbUsername= "root";
- $dbPassword= "";
- $dbName= "DiagnosticSystem";
-
- $conn= mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
--- Admin/Dashboard/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    echo "<script>alert('Logged out successfully');window.location='../Login';</script>";
